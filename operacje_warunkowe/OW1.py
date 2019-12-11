ebf6c7fb738a28388a067f6fc7e6d362008d134a
Mirek <mirek@info-tur.pl>
2019-12-11 17:15:13 +0100

    test

--- operacje_warunkowe/OW1.py
+++
@@ -1,3 +0,0 @@
-wiek = int(input("Podaj Twój wiek: "))
-
-print("dorosły" if wiek >= 18 else "nieletni")
--- operacje_warunkowe/OW3.py
+++
@@ -1,9 +0,0 @@
-a = input("Podaj pierwszą wartość: ")
-b = input("Podaj drugą wartość: ")
-
-if a > b:
-    print("Liczba a jest większa")
-elif a == b:
-    print("Liczby są równe")
-else:
-    print("Liczba b jest większa")
