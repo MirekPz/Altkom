fb4deae8cc291c866eb10b7dd9d6da7c89d24dd4
Mirek <mirek@info-tur.pl>
2019-12-11 17:12:32 +0100

    test

--- cwiczenie_3-1/procent.py
+++
@@ -1,7 +0,0 @@
-kapital = int(input("Kwota lokaty: "))
-procent = int(input("Oprocentowanie roczne: "))
-lata = int(input("Na ile lat lokata: "))
-
-kwota = round(kapital * (1 + procent/100)**lata, 2)
-
-print(kwota)
